fc77ca72767d4dcd00611ac4737fd29bba0842bf
ylwei <weiyaling@hd123.com>
2018-12-28 21:16:10 +0800

    update

---
+++ app/src/main/java/com/grade/unit/callback/OnErrorCallback.java
@@ -0,0 +1,11 @@
+package com.grade.unit.callback;
+
+/**
+ * OnErrorCallback :
+ * <p>
+ * </> Created by ylwei on 2018/12/28.
+ */
+public interface OnErrorCallback {
+
+  void failure(String errorMsg);
+}
